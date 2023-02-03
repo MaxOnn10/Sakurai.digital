b02625ad079ef5b6c2e13a3bb08a9c135a711889
MaxOnn10 <maks.melenevskiy@gmail.com>
2023-02-04 00:21:19 +0200

    file update

--- js/main.js
+++ js/main.js
@@ -10,14 +10,6 @@ for (i = 0; i < accordion.length; i++) {
   })
 }
 
-//game-accordion
-const gameAccordion = document.getElementsByClassName('features__list-item');
-
-for (i = 0; i < gameAccordion.length; i++) {
-  gameAccordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
-}
 Fancybox.bind("[data-fancybox]", {
   autoFocus: false,
   dragToClose: false,
@@ -51,59 +43,3 @@ document.querySelector('.header__menu-list').addEventListener("click", function 
   document.querySelector(".header__menu-item").classList.remove("active"),
   body.classList.remove("disable-scroll");
 });
-
-//tabs
-const buttonsContainer = document.querySelector('.tabs__buttons');
-const buttonItem = buttonsContainer.querySelectorAll('.tabs__button')
-const tabs             = document.querySelector('.tabs__content');
-
-buttonsContainer.addEventListener('click', event => {
-  
-	let index = event.target.closest('.tabs__button').dataset.value;
-  let target = event.target;
-  tabs.querySelector('.active').classList.remove('active');
-  tabs.querySelector(`.tab--${index}`).classList.add('active');
-
-  if (target.classList.contains('tabs__button')){
-    for (let i = 0; i < buttonItem.length; i++) {
-      buttonItem[i].classList.remove('active');
-    }
-    target.classList.add('active');
-  }
-
-});
-const element = document.getElementById("image-compare");
-  
-const options = {
-
-  // UI Theme Defaults
-
-  controlColor: "#fff",
-  controlShadow: false,
-  addCircle: false,
-  addCircleBlur: false,
-
-  // Label Defaults
-
-  showLabels: false,
-  labelOptions: {
-    before: 'Before',
-    after: 'After',
-    onHover: false
-  },
-
-  // Smoothing
-
-  smoothing: true,
-  smoothingAmount: 100,
-
-  // Other options
-
-  hoverStart: false,
-  verticalMode: false,
-  startingPoint: 50,
-  fluidMode: false
-};
-  
-// Add your options object as the second argument
-const viewer = new ImageCompare(element, options).mount();
